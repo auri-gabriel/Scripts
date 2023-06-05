8ac70633b2a41ca895bb293bea7dd3da98f3f211
Auri Gabriel <ririmello.a@gmail.com>
2023-06-04 22:53:43 -0300

    Initial commit

---
+++ sync_vimwiki.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Change directory to your Vim Wiki directory
+cd /home/aurigabriel/vimwiki/
+
+# Add all changes and commit with a message
+git add .
+git commit -m "Auto-sync Vim Wiki $(date -R)"
+
+# Push the changes to the remote repository
+git push
+
