aec8f05d4420e5cd6f4819e24a7a1716b05029b1
Auri Gabriel <ririmello.a@gmail.com>
2023-07-07 11:49:12 -0300

    feat: add a pull on the vimwiki script

--- sync_vimwiki.sh
+++ sync_vimwiki.sh
@@ -3,6 +3,9 @@
 # Change directory to your Vim Wiki directory
 cd /home/aurigabriel/vimwiki/
 
+# Pull the latest changes from the remote repository
+git pull
+
 # Add all changes and commit with a message
 git add .
 git commit -m "Auto-sync Vim Wiki $(date -R)"
